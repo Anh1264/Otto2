2fe5c12f5e23449dcea0cd5c189fa633493f9f3e
Anh1264 <vuquocanh12052007@gmail.com>
2024-06-21 16:12:32 +0700

    change url arg to str

--- backend/robot/urls.py
+++ backend/robot/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns= [
     path('', views.RobotMixinView.as_view()),
     path('create/', views.RobotMixinView.as_view()),
-    path('<int:robot_id>/', views.RobotMixinView.as_view()),
-    path('<int:robot_id>/update/', views.RobotMixinView.as_view()),
-    path('<int:robot_id>/destroy/', views.RobotMixinView.as_view()),
+    path('<str:robot_id>/', views.RobotMixinView.as_view()),
+    path('<str:robot_id>/update/', views.RobotMixinView.as_view()),
+    path('<str:robot_id>/destroy/', views.RobotMixinView.as_view()),
 ]
